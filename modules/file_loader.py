ec336f47b7be14c4ed2aa5cb2fad4fe90d591a64
aayush-navsariwala <aayush.nav@gmail.com>
2025-10-09 21:55:00 +0200

    Commit 13 - Updated file_loader.py to not be case-sensitive for image formats

--- modules/file_loader.py
+++ modules/file_loader.py
@@ -2,13 +2,15 @@ def detect_file_type(file_path):
     with open(file_path, 'rb') as f:
         header = f.read(10)
 
-    if file_path.endswith(".docx") and header.startswith(b'PK'):
+    ext = file_path.lower().split('.')[-1]
+
+    if ext == "docx" and header.startswith(b'PK'):
         return "docx"
-    elif file_path.endswith(".pdf") and header.startswith(b'%PDF'):
+    elif ext == "pdf" and header.startswith(b'%PDF'):
         return "pdf"
-    elif file_path.endswith(".jpg") and header[0:2] == b'\xFF\xD8':
+    elif ext in ["jpg", "jpeg"] and header[0:2] == b'\xFF\xD8':
         return "jpg"
-    elif file_path.endswith(".png") and header.startswith(b'\x89PNG'):
+    elif ext == "png" and header.startswith(b'\x89PNG'):
         return "jpg"  
     else:
         return "unknown"
