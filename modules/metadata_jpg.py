3f1d760dbf710f2c58dd4d84afd417e2fe43ac34
aayush-navsariwala <aayush.nav@gmail.com>
2025-10-09 20:28:51 +0200

    Commit 11 - Updated metadata_jpg.py to take in more information from the camera it was taken on

--- modules/metadata_jpg.py
+++ modules/metadata_jpg.py
@@ -3,7 +3,9 @@ def extract_metadata(file_path):
         "file_type": "image",
         "camera_model": "Unknown",
         "datetime": "Unknown",
-        "software": "Unknown"
+        "software": "Unknown",
+        "created_by": "Unknown",
+        "modified_by": "Unknown"
     }
 
     try:
@@ -14,8 +16,8 @@ def extract_metadata(file_path):
         if exif_start == -1:
             return metadata  
 
-        tiff_header_start = exif_start + 10  
-        endian = data[tiff_header_start:tiff_header_start + 2]
+        tiff_start = exif_start + 10  
+        endian = data[tiff_start:tiff_start + 2]
 
         if endian == b'II':
             byte_order = 'little'
@@ -24,28 +26,29 @@ def extract_metadata(file_path):
         else:
             return metadata
 
-        num_tags = int.from_bytes(data[tiff_header_start + 8:tiff_header_start + 10], byte_order)
-
-        ifd_offset = tiff_header_start + 10
-        entry_size = 12  
+        num_tags = int.from_bytes(data[tiff_start + 8:tiff_start + 10], byte_order)
+        entry_size = 12
+        ifd_offset = tiff_start + 10
 
         for i in range(num_tags):
             entry_offset = ifd_offset + i * entry_size
             tag = int.from_bytes(data[entry_offset:entry_offset + 2], byte_order)
 
-            if tag == 0x0131:  
-                val_offset = int.from_bytes(data[entry_offset + 8:entry_offset + 12], byte_order)
-                software = extract_ascii_string(data, tiff_header_start + val_offset)
-                metadata["software"] = software
+            if tag == 0x0110:  
+                offset = int.from_bytes(data[entry_offset + 8:entry_offset + 12], byte_order)
+                model = extract_ascii_string(data, tiff_start + offset)
+                metadata["camera_model"] = model
+                metadata["created_by"] = model
 
-            elif tag == 0x0110:  
-                val_offset = int.from_bytes(data[entry_offset + 8:entry_offset + 12], byte_order)
-                camera_model = extract_ascii_string(data, tiff_header_start + val_offset)
-                metadata["camera_model"] = camera_model
+            elif tag == 0x0131:  
+                offset = int.from_bytes(data[entry_offset + 8:entry_offset + 12], byte_order)
+                sw = extract_ascii_string(data, tiff_start + offset)
+                metadata["software"] = sw
+                metadata["modified_by"] = sw
 
-            elif tag == 0x9003:  
-                val_offset = int.from_bytes(data[entry_offset + 8:entry_offset + 12], byte_order)
-                dt = extract_ascii_string(data, tiff_header_start + val_offset)
+            elif tag == 0x9003: 
+                offset = int.from_bytes(data[entry_offset + 8:entry_offset + 12], byte_order)
+                dt = extract_ascii_string(data, tiff_start + offset)
                 metadata["datetime"] = dt
 
     except Exception as e:
@@ -55,7 +58,7 @@ def extract_metadata(file_path):
 
 def extract_ascii_string(data, start_index):
     result = b''
-    for i in range(0, 100):  
+    for i in range(0, 100):
         byte = data[start_index + i]
         if byte == 0:
             break
