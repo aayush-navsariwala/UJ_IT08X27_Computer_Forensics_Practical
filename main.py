8d7421d430b1d4aff5a9f08fd5cf075fdbfb822b
aayush-navsariwala <aayush.nav@gmail.com>
2025-10-09 15:32:03 +0200

    Commit 1 - Initial commit with base folders

---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+from modules.file_loader import detect_file_type
+from modules import metadata_docx, metadata_pdf, metadata_jpg
+from modules.anomaly_checker import check_anomalies
+from modules.report_generator import generate_report
+
+def main():
+    print("=== Digital Metadata Forensics Tool ===")
+    file_path = input("Enter the path to the file you want to scan: ")
+
+    if not os.path.exists(file_path):
+        print("Error: File not found.")
+        return
+
+    file_type = detect_file_type(file_path)
+
+    if file_type == "docx":
+        metadata = metadata_docx.extract_metadata(file_path)
+    elif file_type == "pdf":
+        metadata = metadata_pdf.extract_metadata(file_path)
+    elif file_type == "jpg":
+        metadata = metadata_jpg.extract_metadata(file_path)
+    else:
+        print("Unsupported file type.")
+        return
+
+    anomalies = check_anomalies(metadata)
+    generate_report(file_path, metadata, anomalies)
+
+if __name__ == "__main__":
+    main()
+    
